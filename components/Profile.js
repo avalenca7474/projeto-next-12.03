9394172d92cb3a2581d8d238803931117ff44e89
avalenca74 <adriano.00000860049@unicap.br>
2026-03-12 23:29:10 -0300

    Envia os componentes do perfil para o GitHub

---
+++ components/Profile.js
@@ -0,0 +1,18 @@
+export default function Profile() {
+  return (
+    <>
+      <img
+        src="https://i.imgur.com/sOrsw1I.png"
+        alt="Adriano Valença"
+        width={300} 
+        height={100}
+        style={{
+          borderRadius: '50%', 
+          border: '4px solid #166534', 
+        }}
+      />
+      <h2>Adriano Valença</h2>
+      <p>Estudante de Sistemas para Internet na UNICAP.</p>
+    </>
+  );
+}
